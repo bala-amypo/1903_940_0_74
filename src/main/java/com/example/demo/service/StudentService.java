d53f0218a72d51dac30e5aa07caceae7f12edae8
bala-amypo <bala@amypo.in>
2025-12-19 06:34:26 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -2,9 +2,10 @@ package com.example.demo.service;
 
 import com.example.demo.entity.Student;
 
-public interface StudentService{
+public interface StudentService {
 
     Student saveStudent(Student student);
-    Student getStudent
-
+    Student getStudentById(Long id);
+    Student updatedata(Long id,Student student);
+    Student deletdata(Long id);
 }
--- src/main/java/com/example/serviceimpl/StudentServiceimpl.java
+++ src/main/java/com/example/serviceimpl/StudentServiceimpl.java
@@ -1,4 +1,4 @@
-package com.example.demo Serviceimpl;
+package com.example.demo.Serviceimpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -6,14 +6,33 @@ import org.springframework.stereotype.Service;
 import com.example.demo.entity.Student;
 import com.example.demo.repository.StudentRepository;
 import com.example.demo.service.StudentService;
+import com.example.demo.exception.ResourceNotFoundException;
 
 @Service
-public class StudentServiceImple implements StudentService  {
+public class StudentServiceImple implements StudentService {
     @Autowired
     StudentRepository studentRepository;
 
     public Student saveStudent(Student student){
         return studentRepository.save(student);
+    }
+    
+    public Student getStudentById(Long id){
+        return studentRepository.findById(id)
+        .orElseThrow(()-> new ResourceNotFoundException("Student not found"));
+    }
+    public Student updatedata(Long id,Student student){
+        Student exits=getStudentById(id);
+        exits.setName(student.getName());
+        exits.setEmail(student.getEmail());
+        return studentRepository.save(exits);
+        // .orElseThrow(()-> new ResourceNotFoundException("Student not found"));
+    }
+
+    public Student deletdata(Long id){
+        Student student=getStudentById(id);
+        studentRepository.delete(student);
+        return student;
     }
 
 }
